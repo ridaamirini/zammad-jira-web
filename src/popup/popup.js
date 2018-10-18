1191a43aa73388503f1fca9aed8a97de12247411
Rida Amirini <ridaamirini@rs-apps.de>
2018-10-18 19:26:07 +0200

    Initial commit

---
+++ src/popup/popup.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+import store from '../store'
+import router from './router'
+
+global.browser = require('webextension-polyfill');
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(App)
+});
---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="loader" v-show="showLoader">
+    <half-circle-spinner
+      :animation-duration="1000"
+      :size="60"
+      color="#0747a6"
+    />
+  </div>
+</template>
+
+<script>
+  import { HalfCircleSpinner } from 'epic-spinners'
+
+  export default {
+    components: {
+        HalfCircleSpinner
+    },
+    data() {
+      return {
+          showLoader: true
+      }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showLoader = false;
+
+            if (window.hasOwnProperty('Spine') &&
+                window.hasOwnProperty('App') &&
+                window.App.Browser.check()) {
+                console.log('Zammad gefunden!');
+            }
+        }, 2000);
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .loader {
+    position: absolute;
+    top: 170px;
+    right: 170px;
+  }
+
+  p {
+    font-size: 20px;
+  }
+</style>
